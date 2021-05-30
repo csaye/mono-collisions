762c9d21303f29c615c7ab07ceb1290af5cf0d39
Cooper Saye <coopersaye@gmail.com>
2021-05-29 18:45:20 -0700

    Implement collision detection.

--- MonoCollisions/Objects/ObjectManager.cs
+++ MonoCollisions/Objects/ObjectManager.cs
@@ -31,5 +31,53 @@ namespace MonoCollisions.Objects
                 gameObject.Draw(game);
             }
         }
+
+        // Returns nearest empty position for given object moving to given position
+        public Vector2 NearestEmptyPosition(GameObject movingObject, Vector2 newPosition)
+        {
+            // Get new bounds
+            Rectangle newBounds = new Rectangle(newPosition.ToPoint(), movingObject.Size.ToPoint());
+
+            // For each object
+            foreach (GameObject gameObject in gameObjects)
+            {
+                // Skip self
+                if (gameObject == movingObject) continue;
+
+                // If object intersects with new bounds
+                Rectangle objBounds = gameObject.Bounds;
+                if (objBounds.Intersects(newBounds))
+                {
+                    // Get object centers
+                    Point objCenter = gameObject.Bounds.Center;
+                    Point newCenter = newBounds.Center;
+
+                    // Get horizontal and vertical displacements
+                    float displacementX = Math.Abs(objCenter.X - newCenter.X);
+                    float displacementY = Math.Abs(objCenter.Y - newCenter.Y);
+
+                    // If greater horizontal displacement
+                    if (displacementX > displacementY)
+                    {
+                        // If right of object
+                        if (newCenter.X > objCenter.X) newPosition.X = newBounds.X = objBounds.Right;
+                        // If left of object
+                        else newPosition.X = newBounds.X = objBounds.Left - newBounds.Width;
+                    }
+                    // If greater vertical displacement
+                    else
+                    {
+                        // If below object
+                        if (newCenter.Y > objCenter.Y) newPosition.Y = newBounds.Y = objBounds.Bottom;
+                        // If above object
+                        else newPosition.Y = newBounds.Y = objBounds.Top - newBounds.Height;
+
+                    }
+                }
+            }
+
+            // Return corrected new position
+            return newPosition;
+        }
     }
 }
