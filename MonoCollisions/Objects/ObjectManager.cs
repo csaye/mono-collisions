8fab4993414e5b5930ba47890d62d38ce3ddef9c
Cooper Saye <coopersaye@gmail.com>
2021-05-29 18:44:40 -0700

    Create ObjectManager class.

---
+++ MonoCollisions/Objects/ObjectManager.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace MonoCollisions.Objects
+{
+    public class ObjectManager
+    {
+        public List<GameObject> gameObjects = new List<GameObject>();
+
+        public ObjectManager() { }
+
+        public void AddObject(GameObject gameObject) => gameObjects.Add(gameObject);
+
+        public void Update(Game1 game, float delta)
+        {
+            // For each object
+            foreach (GameObject gameObject in gameObjects)
+            {
+                // Update object
+                gameObject.Update(game, delta);
+            }
+        }
+
+        public void Draw(Game1 game)
+        {
+            // For each object
+            foreach (GameObject gameObject in gameObjects)
+            {
+                // Draw object
+                gameObject.Draw(game);
+            }
+        }
+    }
+}
